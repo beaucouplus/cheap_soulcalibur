4740de558ad19df4421085604f7ed2eee03fd67d
beaucouplus <maxime@beaucouplus.com>
2018-02-26 17:53:24 +0100

    bootstrap intégré dans js + css / routes testées

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,4 +6,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "root should be defined" do
+    get root_path
+    assert_response :success
+  end
+
+  test "root should return static_pages_home_path" do
+    assert_routing root_path, controller: 'static_pages', action: 'home'
+  end
+
 end
