229af5a1149ad6ab19a7ed6f75fd66844b9bace2
beaucouplus <maxime@beaucouplus.com>
2018-02-27 12:04:04 +0100

    créé premiers tests - je dois voir comment gérer la partie upload de fichier et les tests qui vont avec

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180227101416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "players", force: :cascade do |t|
+    t.string "player_name"
+    t.integer "life_points"
+    t.integer "attack_points"
+    t.string "profile_pic"
+    t.integer "experience"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @player = Player.create(player_name: "Mitsurugi", life_points: 100, attack_points: 100, profile_pic: "test/fixtures/files/mitsurugi.jpg",experience: 0)
+  end
+
+  test "empty player should be invalid" do
+    player = Player.new
+    refute player.valid?
+  end
+
+  test "player should be valid with all parameters" do
+    assert @player.valid?
+  end
+
+  test "player should be invalid without player_name" do
+    @player.player_name = nil
+    refute @player.valid?
+  end
+
+  test "player_name should be unique" do
+    player_1 = Player.create(player_name: "Mitsurugi", life_points: 100, attack_points: 100, profile_pic: "test/fixtures/files/mitsurugi.jpg",experience: 0)
+    @player.player_name = "Mitsurugi"
+    refute @player.valid?
+  end
+
+  test "player should be invalid without life points" do
+    @player.life_points = nil
+    refute @player.valid?
+  end
+
+  test "player should be invalid with out of range life points" do
+    @player.life_points = 130
+    refute @player.valid?
+  end
+
+  test "player should be invalid without attack points" do
+    @player.attack_points = nil
+    refute @player.valid?
+  end
+
+  test "player should be invalid with out of range attack points" do
+    @player.attack_points = 130
+    refute @player.valid?
+  end
+
+  test "player should be invalid without profile pic" do
+    @player.profile_pic = nil
+    refute @player.valid?
+  end
+
+  test "player should be invalid with out of range experience" do
+    @player.experience = -30
+    refute @player.valid?
+  end
+
+  # test de paperclip ?
+
 end
